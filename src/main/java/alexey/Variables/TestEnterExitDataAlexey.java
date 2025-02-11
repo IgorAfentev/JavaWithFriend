b754b0a74df8544decddc25c0f34da518368bb2d
Alexey Sergeev <alekssergeyev1995@gmail.com>
2025-02-11 19:08:09 +0300

    comment

--- src/main/java/alexey/Variables/TestEnterExitDataAlexey.java
+++ src/main/java/alexey/Variables/TestEnterExitDataAlexey.java
@@ -3,20 +3,20 @@ package alexey.Variables;
 import java.util.Scanner;
 
 public class TestEnterExitDataAlexey {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.print("Привет, мир!\n");
         System.out.print("Привет, всем!\n");
         int x = 10;
         int y = 22;
         System.out.println("x = " + x + "; y = " + y);
         Scanner in = new Scanner(System.in);
-        System.out.printf("Enter name:");
+        System.out.println("Enter name:");
         String name = in.nextLine();
-        System.out.printf("Enter age:");
+        System.out.println("Enter age:");
         int age = in.nextInt();
-        System.out.printf("Enter height:");
-        float heihgt = in.nextFloat();
-        System.out.println("Name:" + name + ", Age: " + age + ", Height: " + heihgt + "cm");
+        System.out.println("Enter height:");
+        float height = in.nextFloat();
+        System.out.println("Name:" + name + ", Age: " + age + ", Height: " + height + "cm");
         in.close();
     }
 }
